51cae039826f1df285094b18bd8490c1a27834eb
Jubril <jubrilbucknor@gmail.com>
2022-10-12 19:27:37 +0100

    First look of the calculator

---
+++ script.js
@@ -0,0 +1,10 @@
+const textarea = document.querySelector(".text-area");
+const dot = document.querySelectorAll(".dot");
+const dot1 = document.querySelectorAll(".dot1");
+const dot2 = document.querySelectorAll(".dot2");
+const dott = document.querySelector(".dott");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e) =>{
+    textarea.disables = "true"
+})
