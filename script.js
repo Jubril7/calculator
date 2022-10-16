7040859385ace2ecb9f4a22494308040a2ceecc6
Jubril <jubrilbucknor@gmail.com>
2022-10-16 07:37:52 +0100

    numbers working when clicked

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const textarea = document.querySelector(".text-area");
-const dot = document.querySelectorAll(".dot");
-const dot1 = document.querySelectorAll(".dot1");
-const dot2 = document.querySelectorAll(".dot2");
-const dott = document.querySelector(".dott");
-const btn = document.querySelector(".btn");
+const screenDisplay = document.querySelector(".screen-display");
+const dataNumbers = document.querySelectorAll(".numbers");
+const dataOperation = document.querySelector(".operation");
+const dataOperations = document.querySelectorAll(".operations");
+const dataNumber = document.querySelector(".number");
+const btnOn = document.querySelector(".btn-on");
+const btnOff = document.querySelector(".btn-off");
+
+
+
+
+dataNumbers.forEach(function(button){
+    button.addEventListener("click", function(){
+      let buttonValue = this.textContent;
+        screenDisplay.innerHTML = (screenDisplay.innerHTML + buttonValue).substr(0, 10);
+    })
+})
+dataNumber.addEventListener("click", function(){
+    let buttonValue = this.textContent;
+    screenDisplay.innerHTML = (screenDisplay.innerHTML + buttonValue).substr(0, 10); 
+})
+
+
+
 
 
 
