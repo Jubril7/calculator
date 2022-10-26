9493ccc053b5242be6b83476107291defb4b7fa8
Jubril <jubrilbucknor@gmail.com>
2022-10-26 19:36:39 +0100

    Switch button now a single button and works

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const dataNumbers = document.querySelectorAll(".numbers");
 const dataOperation = document.querySelector(".operation");
 const dataOperations = document.querySelectorAll(".operations");
 const dataNumber = document.querySelector(".number");
-const btnOn = document.querySelector(".btn-on");
-const btnOff = document.querySelector(".btn-off");
+const btnOnOff = document.querySelector(".btn-onoff");
 const plusOrMinus = document.querySelector(".operation1");
 const dataPercent = document.querySelector(".operation2");
 const decimal = document.querySelector(".decimal");
-const dataEquals = document.querySelector(".operations1")
+const dataEquals = document.querySelector(".operations1");
+const prevScreenDisplay = document.querySelector(".screen-display-pre");
+
+screenDisplay.innerHTML = screenDisplay.innerHTML.toLocaleString("en-US")
+
 
 dataNumbers.forEach(function(button){
     button.addEventListener("click", function(){
       let buttonValue = this.textContent;
-        screenDisplay.innerHTML = (screenDisplay.innerHTML += buttonValue).replace(/^0+/, '').substr(0, 9);   
+        screenDisplay.innerHTML = (screenDisplay.innerHTML += buttonValue).replace(/^0+/, '').substr(0, 9);
     })
 })
 
@@ -24,95 +27,126 @@ dataNumber.addEventListener("click", function(){
 
 decimal.addEventListener("click", function(){
   if(screenDisplay.innerHTML.indexOf(".") === -1){
-    screenDisplay.innerHTML += "."
+    screenDisplay.innerHTML += ".";
   }
 })  
 
 dataOperation.addEventListener("click", function(){
     screenDisplay.innerHTML = "0"
+    prevScreenDisplay.innerHTML = ""
+
     // dataOperation.innerText = "AC"
-//   screenDisplay.innerHTML = screenDisplay.innerHTML.slice(0, -1)  
+    // screenDisplay.innerHTML = screenDisplay.innerHTML.slice(0, -1)  
 })
 
 dataOperations.forEach(function(button){
     button.addEventListener("click", function(e){
         button.style.backgroundColor = "rgb(246, 230, 216)"
-        
         let buttonValue = this.textContent;
-        buttonValue = e.target.innerText;
-        console.log('Button text is ', buttonValue);
-        if(buttonValue ===  "x"){
-            buttonValue = "*";
-
+            buttonValue = e.target.innerText;
+        if(buttonValue === "x"){
+                prevScreenDisplay.innerHTML = (screenDisplay.innerHTML).replace(/^0+/, '')   
+                screenDisplay.innerHTML = "0" 
+            dataEquals.addEventListener("click", function(){
+                screenDisplay.innerHTML = prevScreenDisplay.innerHTML * screenDisplay.innerHTML    
+            })
+        } else if(buttonValue === "+"){
+                prevScreenDisplay.innerHTML = (screenDisplay.innerHTML).replace(/^0+/, '')   
+                screenDisplay.innerHTML = "0" 
+            dataEquals.addEventListener("click", function(){
+                    screenDisplay.innerHTML = prevScreenDisplay.innerHTML + screenDisplay.innerHTML   
+            })
         } else if(buttonValue === "-"){
-            buttonValue = "-"
-        
-        
-        }else if(buttonValue === "+"){
-            buttonValue = "+"
-        
-        
-        }else (buttonValue === "&#xF7")
-            buttonValue = "/"
-        
-        
-        
-
+                prevScreenDisplay.innerHTML = (screenDisplay.innerHTML).replace(/^0+/, '')   
+                screenDisplay.innerHTML = "0"
+            dataEquals.addEventListener("click", function(){
+                screenDisplay.innerHTML = prevScreenDisplay.innerHTML - screenDisplay.innerHTML 
+            }) 
+        } else if(buttonValue === "÷"){
+                prevScreenDisplay.innerHTML = (screenDisplay.innerHTML).replace(/^0+/, '')   
+                screenDisplay.innerHTML = "0" 
+            dataEquals.addEventListener("click", function(){
+                screenDisplay.innerHTML = prevScreenDisplay.innerHTML / screenDisplay.innerHTML 
+            }) 
+        }
     })
 })
 
-btnOn.addEventListener("click", function(){
-    dataNumber.disabled = false;
-    dataOperation.disabled = false;
-    dataPercent.disabled = false;
-    plusOrMinus.disabled = false;
-    dataNumber.style.color = "white"
-    dataOperation.style.color = "white"
-    dataPercent.style.color = "white" 
-    plusOrMinus.style.color = "white" 
+
+btnOnOff.addEventListener("click", function(e){
+    let buttonValue = this.textContent;
+        buttonValue = e.target.innerText;
+    if(buttonValue === "OFF"){
+        dataNumber.disabled = true;
+        dataOperation.disabled = true;
+        dataPercent.disabled = true;
+        plusOrMinus.disabled = true;
+        decimal.disabled = true;
+        dataEquals.disabled = true;
+        dataNumber.style.color = "darkgrey"
+        dataOperation.style.color = "darkgrey"
+        dataPercent.style.color = "darkgrey" 
+        plusOrMinus.style.color = "darkgrey"
+        decimal.style.color = "darkgrey" 
+        dataEquals.style.color = "darkgrey" 
+        btnOnOff.innerText = "ON"
+        btnOnOff.style.backgroundColor = "green"
+        screenDisplay.innerHTML = " "
+    } else if(buttonValue === "ON"){
+        dataNumber.disabled = false;
+        dataOperation.disabled = false;
+        dataPercent.disabled = false;
+        plusOrMinus.disabled = false;
+        decimal.disabled = false;
+        dataEquals.disabled = false;
+        dataNumber.style.color = "white"
+        dataOperation.style.color = "white"
+        dataPercent.style.color = "white" 
+        plusOrMinus.style.color = "white"
+        decimal.style.color = "white"
+        dataEquals.style.color = "white"
+        btnOnOff.innerText = "OFF"
+        btnOnOff.style.backgroundColor = "red"
+        screenDisplay.innerHTML = "0"
+    }
+
+    
 })
 
 
-btnOff.addEventListener("click", function(){
-    dataNumber.disabled = true;        
-    dataOperation.disabled = true;
-    dataPercent.disabled = true;
-    plusOrMinus.disabled = true;
-    dataNumber.style.color = "darkgrey"
-    dataOperation.style.color = "darkgrey"
-    dataPercent.style.color = "darkgrey"
-    plusOrMinus.style.color = "darkgrey"
-
-
-})
-
-btnOn.addEventListener("click", function(){
+btnOnOff.addEventListener("click", function(e){
     dataNumbers.forEach(function(button){
-        button.disabled = false;
-        button.style.color = "white"
+        let buttonValue = this.textContent;
+            buttonValue = e.target.innerText; 
+        if(buttonValue === "ON"){    
+            button.disabled = true;
+            button.style.color = "darkgrey"
+        } else if(buttonValue === "OFF"){
+            button.disabled = false;
+            button.style.color = "white"
+        }     
     })
 })
 
-btnOff.addEventListener("click", function(){
-    dataNumbers.forEach(function(button){
-        button.disabled = true;
-        button.style.color = "darkgrey"
-    })
-})
 
-btnOn.addEventListener("click", function(){
+
+
+btnOnOff.addEventListener("click", function(e){
     dataOperations.forEach(function(button){
-        button.disabled = false;
-        button.style.color = "white"
+        let buttonValue = this.textContent;
+            buttonValue = e.target.innerText; 
+        if(buttonValue === "ON"){    
+            button.disabled = true;
+            button.style.color = "darkgrey"
+        } else if(buttonValue === "OFF"){
+                button.disabled = false;
+                button.style.color = "white"
+        }
     })
 })
 
-btnOff.addEventListener("click", function(){
-    dataOperations.forEach(function(button){
-        button.disabled = true;
-        button.style.color = "darkgrey"
-    })
-})
+
+
 
 plusOrMinus.addEventListener("click", function(){
     screenDisplay.innerHTML = screenDisplay.innerHTML * -1
